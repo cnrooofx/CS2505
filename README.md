1e8eebf7c6fd3067ebb8522ba680379f2124ae07
Conor Fox <conor@conorfox.com>
2021-03-19 12:40:07 +0000

    Lab1 (#2) (#3)
    * Lab 1
    
    * Lab 1 base questions working
    
    * Comments
    
    * Update README.md
    
    * Send time strinng to client

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CS2505
 Network Computing
+
+## Lab 1
+
+Introduction to socket programming using loopback interface.
--- lab1/client_solution.py
+++ lab1/client_solution.py
@@ -12,16 +12,18 @@ sock.connect((host_name, port))
 
 try:
     message = input("Type message to send: ")  # Take input from terminal
-    print("sending '%s'" % message)
+    print("sending '{}'".format(message))
     sock.sendall(message.encode())
 
     amount_received = 0
-    amount_expected = len(message)
+    amount_expected = len(message) + 26
 
+    reply = ""
     while amount_received < amount_expected:
         data = sock.recv(16).decode()
         amount_received += len(data)
-        print("received '%s'" % data)
+        reply += data
+    print("received '{}'".format(reply))
 
 finally:
     print("closing socket")
