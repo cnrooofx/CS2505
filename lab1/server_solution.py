0666feff7b67768a5eb554bf8a390ba850bad00a
Conor Fox <conor@conorfox.com>
2021-03-19 12:28:56 +0000

    Send time strinng to client

--- lab1/server_solution.py
+++ lab1/server_solution.py
@@ -17,22 +17,24 @@ while True:
     connection, client_address = sock.accept()
     try:
         print("connection from", client_address)
+        time = "[{}] ".format(datetime.now().strftime("%c"))
+        # %c is local datetime
         message = ""
         while True:
             data = connection.recv(16).decode()
-            if data:
-                message += data
-                print("received '%s'" % data)
-                print("sending data back to the client")
-                connection.sendall(data.encode())
-            else:
+            if not data:
                 print("no more data from", client_address)
                 break
+            elif message == "":
+                connection.sendall(time.encode())
+            message += data
+            print("received '{}'".format(data))
+            print("sending data back to the client")
+            connection.sendall(data.encode())
 
         # Append the current date and time with the message to the logfile
         with open("log.txt", "a") as log:
-            time = datetime.now().strftime("%c")  # %c is local datetime
-            log_message = "[{}] {}\n".format(time, message)
+            log_message = "{}{}\n".format(time, message)
             log.write(log_message)
 
     finally:
